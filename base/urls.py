772e99312670bd189ffc670b77503fed0e0c21eb
Tahmid Rahman <tahmid11q@gmail.com>
2021-07-05 23:06:09 +0600

    detail & create function

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskCreate, TaskDetail, TaskList
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>', TaskDetail.as_view(), name='task'),
+    path('task-create', TaskCreate.as_view(), name='task-create'),
 ]
